ded156b254ec8eb228c2f5a9f669da7f365d24e1
sinofgreed <misha415@list.ru>
2024-09-19 21:51:52 +0500

    Add echo bot on Github

---
+++ src/main/java/org/example/Telegrambot.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Telegrambot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "B0T_T3STbot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7359784003:AAEpT3A6TJaEtV9kiv68mTfKz0BM18MNKDM";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        String chatId = update.getMessage().getChatId().toString();
+        String text = update.getMessage().getText();
+
+        String newText = new StringBuilder(text).reverse().toString();
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(newText);
+
+        try {
+            this.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
