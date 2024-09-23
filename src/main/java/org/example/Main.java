6a7fe9005489e9f040c1c77c0b1b77a7ff3f2434
unknown <bivaetctoskazat123@gmail.com>
2024-09-23 15:39:02 +0500

    Test is over

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main{
     public static void main(String[] args) throws TelegramApiException {
-//        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-//        telegramBotsApi.registerBot(new Telegrambot());
-        System.out.println("test");
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new Telegrambot());
     }
 }
