2cab75e63c7114c2723e4d19389e86312be6b934
unknown <bivaetctoskazat123@gmail.com>
2024-09-30 18:29:22 +0500

    Added BotConfig class

---
+++ src/main/java/org/example/functional/Telegrambot.java
@@ -0,0 +1,40 @@
+package org.example.functional;
+
+import org.example.config.BotConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Telegrambot extends TelegramLongPollingBot {
+
+    private final BotConfig config;
+
+    @Autowired
+    public Telegrambot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String chatID = update.getMessage().getChatId().toString();
+            String sourceText = update.getMessage().getText();
+
+
+        }
+    }
+}
