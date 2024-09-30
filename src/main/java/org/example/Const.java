acce311062f46539095b12817bd10be03eb39000
sinofgreed <misha415@list.ru>
2024-09-30 16:40:02 +0500

    Add file: Configs.java Const.java DatabaseHandler.java . These files are needed to communicate with the database

---
+++ src/main/java/org/example/Const.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Const
+{
+    public static final String USER_TABLE = "users";
+    public static final String USERS_ID = "telegramID";
+    public static final String USERS_HOME_AND_RENOVATION = "HomeAndRenovation";
+    public static final String USERS_TRANSPORT = "Transport";
+    public static final String USERS_FOOD = "Food";
+    public static final String USERS_ENTERTAINMENT = "Entertainment";
+    public static final String USERS_PHARMACIES = "Pharmacies";
+    public static final String USERS_COSMETICS = "Cosmetics";
+    public static final String USERS_ITEMS_OF_CLOTHING = "ItemsOfClothing";
+    public static final String USERS_SUPERMARKETS = "Supermarkets";
+    public static final String USERS_SOUVENIRS = "Souvenirs";
+    public static final String USERS_ELECTRONICS_AND_TECHNOLOGY = "ElectronicsAndTechnology";
+    public static final String USERS_BOOKS = "Books";
+}
