af6180ccd236aaa01a73f78b512204775b1dacd0
unknown <bivaetctoskazat123@gmail.com>
2024-09-30 22:42:47 +0500

    Fixed bugs related to the launch of the bot

--- src/main/java/org/example/functional/Telegrambot.java
+++ src/main/java/org/example/functional/Telegrambot.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class Telegrambot extends TelegramLongPollingBot {
@@ -16,44 +16,55 @@ public class Telegrambot extends TelegramLongPollingBot {
     @Value("${bot.token}")
     private String botToken;
 
+    @Override
     public String getBotUsername() {
-        return botName;
+        return "B0T_T3STbot";
     }
 
+    @Override
     public String getBotToken() {
-        return botToken;
+        return "7359784003:AAEpEvYzu9c2ipB7u7SQJVcmcIAKWxESED";
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        if(update.hasMessage() && update.getMessage().hasText()) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
             long chatID = update.getMessage().getChatId();
             String sourceText = update.getMessage().getText();
 
-            switch (sourceText){
+            switch (sourceText) {
                 case "/start": {
-                    sendStartCommandAnswer(chatID, update.getMessage().getChat().getFirstName());
+                    try {
+                        this.execute(sendStartCommandAnswer(chatID, update.getMessage().getChat().getFirstName()));
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
                 }
                 default:
-                    sendIfUnknownCommand(chatID);
+                    try {
+                        this.execute(sendIfUnknownCommand(chatID));
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
             }
-            System.out.println("Hello");
         }
     }
 
-    private void sendStartCommandAnswer(long chatID, String name){
-        String answerToSend = "Приветствую тебя, дорогой" + name + ". Чем я могу тебе помочь?";
-        sendMessage(chatID, answerToSend);
+    private SendMessage sendStartCommandAnswer(long chatID, String name) {
+        String answerToSend = "Приветствую тебя, дорогой " + name + ". Чем я могу тебе помочь?";
+        return sendMessage(chatID, answerToSend);
     }
-    private void sendIfUnknownCommand(long chatID) {
+
+    private SendMessage sendIfUnknownCommand(long chatID) {
         String answerToSend = "Извини, такую команду я не знаю. Напиши /help, чтобы увидеть полный список команд";
-        sendMessage(chatID, answerToSend);
+        return sendMessage(chatID, answerToSend);
     }
-    private void sendMessage (long chatID, String answerToSend){
+
+    private SendMessage sendMessage(long chatID, String answerToSend) {
         SendMessage newMessage = new SendMessage();
         newMessage.setChatId(String.valueOf(chatID));
         newMessage.setText(answerToSend);
+        return newMessage;
     }
-
 }
